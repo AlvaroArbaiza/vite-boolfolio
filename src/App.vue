7fee61cf93f64876702ad47c4de9f38357a7d94a
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-06-28 15:28:38 +0200

    Scaffolding frontend

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script >
+import { store } from '../store.js';
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+    },
+    created() {
+    },
+    computed: {
+    },
+    methods: {
+    }
+}
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+    <h1>Ciao mondo</h1>
 </template>
 
 <style lang="scss">
